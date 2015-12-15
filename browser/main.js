23c9458ced3a5a1ecda3ab28524d3b6c7e79bcec
nichoth <nichoth@gmail.com>
2015-12-15 13:17:12 -0800

    better stuff

--- browser/main.js
+++ browser/main.js
@@ -2,7 +2,9 @@ var vdom = require('virtual-dom');
 var App = require('../lib/App.js');
 var observ = require('observ');
 
-var state = App();
+var state = App({
+  onSubmit: console.log.bind(console)
+});
 
 var loop = require('main-loop')( state(), App.render, vdom );
 state(loop.update);
