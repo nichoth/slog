46caf6d717f6b284cb3b32a5c120cae60b8301a7
nichoth <nichoth@gmail.com>
2016-01-04 12:17:03 -0800

    clean up

--- server.js
+++ server.js
@@ -1,34 +1,17 @@
 var http = require('http');
 var ecstatic = require('ecstatic')({root: __dirname + '/public'});
-var rpc = require('rpc-stream');
 var routes = ['/', '/new', '/node/:id'];
 var router = require('routes')();
 var fs = require('fs');
 var shoe = require('shoe');
 var multilevel = require('multilevel');
 var liveStream = require('level-live-stream');
-// var slogdb = require('slog-db-level');
-
-// var db = require('level')(__dirname+'/data/db', {
-//   valueEncoding: 'json'
-// });
-// var sdb = slogdb(db);
-//
-// var rpcServer = rpc(sdb);
 
 var db = require('level-sublevel')(require('level')(__dirname+'/data/db', {
   valueEncoding: 'json'
 }));
 db.sublevel('graph', { valueEncoding: 'utf8' });
 liveStream.install(db);
-
-// trying level plugin via level-manifest
-// db.methods = db.methods || {};
-// db.methods.foo = { type: 'async' };
-// db.foo = function(cb) {
-//   cb(null, 'bar');
-// }
-
 multilevel.writeManifest(db, __dirname+'/data/manifest.json');
 
 routes.forEach(function(r) {
@@ -58,6 +41,3 @@ var sock = shoe(function(stream) {
 });
 sock.install(server, '/sock');
 
-// wss = websocket.createServer({server: server}, function(stream) {
-//   stream.pipe( multilevel.server(db) ).pipe(stream);
-// });
